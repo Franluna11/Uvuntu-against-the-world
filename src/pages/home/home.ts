ade60f32c0dc242745256852ec512ab86db0285e
esfraner <esfraner@hotmail.com>
2017-05-05 09:29:34 +0200

    Add files via upload
    Commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,17 @@
+import { NavController } from 'ionic-angular';
+import { luchador } from "./luchador";
+import { Component } from '@angular/core';
+
+@Component({
+  selector: "page-home",
+  templateUrl: 'home.html'
+})
+
+
+export class HomePage {
+ 
+
+}
+
+
+
